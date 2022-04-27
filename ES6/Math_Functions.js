6e138db45c24c9413537f52fe0e05db36aa8ea89
anantthawkar93 <anant1993thawkar@gmail.com>
2022-04-27 11:18:25 +0530

    Added ES6 module codes

---
+++ ES6/Math_Functions.js
@@ -0,0 +1,15 @@
+		const add = (x, y) => {
+		  return x + y;
+		}
+
+		const subtract = (x, y) => {
+		  return x + y;
+		}
+
+		const (x, y) => {
+		  return x + y;
+		}
+
+		const (x, y) => {
+		  return x - y;
+		}
